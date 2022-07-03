3c233e4f0955bf16bd36f4593838e7ea3eef46ad
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-07-03 12:49:48 +0300

    Add test cases

--- src/test/java/com/ak/spring/ApplicationTests.java
+++ src/test/java/com/ak/spring/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ak.spring;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
 class ApplicationTests {
+  @Autowired
+  private CommandLineRunner commandLineRunner;
 
   @Test
   void contextLoads() {
+    Assertions.assertNotNull(commandLineRunner);
   }
 }
 
---
+++ src/test/java/com/ak/spring/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.ak.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = HelloController.class)
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void getHello() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(startsWith("Greetings from Spring Boot")));
+  }
+}

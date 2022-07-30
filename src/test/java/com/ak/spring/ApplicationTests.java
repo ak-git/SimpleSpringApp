2347dbee2bbf3d2d9b33261fdffeaa2f4d04e28c
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-07-30 18:14:07 +0300

    Accessing Data with JPA

--- src/test/java/com/ak/spring/ApplicationTests.java
+++ src/test/java/com/ak/spring/ApplicationTests.java
@@ -2,20 +2,10 @@ package com.ak.spring;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
 class ApplicationTests {
-  @Autowired
-  private CommandLineRunner commandLineRunner;
-
-  @Test
-  void contextLoads() {
-    Assertions.assertNotNull(commandLineRunner);
-  }
-
   @Test
   void testMain() {
     try {

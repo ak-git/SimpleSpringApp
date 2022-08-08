ee2ae4db055307c35029a35295da25cf8ccb492f
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-08-08 19:22:29 +0300

    list of the latest Player's versions

--- src/main/java/com/ak/spring/data/repository/PlayerRepository.java
+++ src/main/java/com/ak/spring/data/repository/PlayerRepository.java
@@ -28,6 +28,12 @@ public interface PlayerRepository extends JpaRepository<Player, RevisionableId> 
       "and (NULLIF(a.firstName, '') IS NOT NULL and NULLIF(a.surName, '') IS NOT NULL) and NULLIF(a.lastName, '') IS NOT NULL")
   Player findByUUID(@Param("uuid") @NonNull UUID uuid);
 
+  /**
+   * Based on <a href="http://sqlfiddle.com/#!9/a6c585/1">SQL Fiddle</a>
+   * and answer <a href="https://stackoverflow.com/a/7745635/808921">SQL select only rows with max value on a column</a>
+   *
+   * @return list of the latest Player's versions
+   */
   @Query("select a from Player a left outer join Player b ON a.uuid = b.uuid AND a.revision < b.revision where b.uuid is null " +
       "and (NULLIF(a.firstName, '') IS NOT NULL and NULLIF(a.surName, '') IS NOT NULL) and NULLIF(a.lastName, '') IS NOT NULL")
   List<Player> findAllPlayers();

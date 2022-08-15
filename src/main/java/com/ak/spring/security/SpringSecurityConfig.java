b7ea38fa34d2ea8f20d5dab2eea0db2c83832128
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-08-15 11:29:33 +0300

    Add enum for roles

--- src/main/java/com/ak/spring/security/SpringSecurityConfig.java
+++ src/main/java/com/ak/spring/security/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.ak.spring.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +19,14 @@ import org.springframework.security.web.csrf.CsrfTokenRepository;
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig {
+  private enum Roles {
+    ADMIN, USER;
+
+    static String[] all() {
+      return Arrays.stream(values()).map(Enum::name).toArray(String[]::new);
+    }
+  }
+
   private static final String USER_PATTERN = "/controller/players/**";
 
   @Bean
@@ -24,16 +35,16 @@ public class SpringSecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(@NonNull HttpSecurity http) throws Exception {
     http
         .httpBasic()
         .and()
         .authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/controller/players/history/**").hasRole("ADMIN")
-        .antMatchers(HttpMethod.GET, USER_PATTERN).hasRole("USER")
-        .antMatchers(HttpMethod.POST, USER_PATTERN).hasRole("USER")
-        .antMatchers(HttpMethod.PUT, USER_PATTERN).hasRole("USER")
-        .antMatchers(HttpMethod.DELETE, USER_PATTERN).hasRole("USER")
+        .antMatchers(HttpMethod.GET, "/controller/players/history/**").hasRole(Roles.ADMIN.name())
+        .antMatchers(HttpMethod.GET, USER_PATTERN).hasAnyRole(Roles.all())
+        .antMatchers(HttpMethod.POST, USER_PATTERN).hasAnyRole(Roles.all())
+        .antMatchers(HttpMethod.PUT, USER_PATTERN).hasAnyRole(Roles.all())
+        .antMatchers(HttpMethod.DELETE, USER_PATTERN).hasAnyRole(Roles.all())
         .and()
         .formLogin().disable()
         .csrf().csrfTokenRepository(csrfTokenRepository());
@@ -44,8 +55,8 @@ public class SpringSecurityConfig {
   public InMemoryUserDetailsManager userDetailsService() {
     PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     return new InMemoryUserDetailsManager(
-        User.withUsername("user").password(encoder.encode("password")).roles("USER").build(),
-        User.withUsername("admin").password(encoder.encode("password")).roles("ADMIN", "USER").build()
+        User.withUsername("user").password(encoder.encode("password")).roles(Roles.USER.name()).build(),
+        User.withUsername("admin").password(encoder.encode("password")).roles(Roles.ADMIN.name()).build()
     );
   }
 }

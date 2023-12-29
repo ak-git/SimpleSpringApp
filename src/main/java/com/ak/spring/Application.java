55f7c302c1521009fd0365ef4a1f21460521637e
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-12-29 18:57:36 +0300

    Add package-info.java

--- src/main/java/com/ak/spring/Application.java
+++ src/main/java/com/ak/spring/Application.java
@@ -2,11 +2,10 @@ package com.ak.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.lang.NonNull;
 
 @SpringBootApplication
 public class Application {
-  public static void main(@NonNull String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 }
